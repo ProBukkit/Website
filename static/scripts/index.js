8dfb17c1acede72dfc87b57738458e1340b49a71
GrandonBroseph <grandonbroseph@gmail.com>
2015-04-20 13:45:53 -0700

    Did some gallery changes, kinda fixed buttons

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -3,9 +3,12 @@ var slider = document.getElementById("slider-list"),
 	x = 0,
 	delay = 5000,
 	speed, timeout;
-function slide(change){
+function slide(change, type){
 	change = change || 1;
-	x ++;
+	type = type || "relative";
+	if(type === "relative")x += change;
+	else if(type === "absolute")x = change;
+	if(x < 0)x = children-1;
 	if(x >= children)x = 0;
 	slider.style.transform = "translateX(-"+x*100+"vw)";
 	clearTimeout(timeout);
